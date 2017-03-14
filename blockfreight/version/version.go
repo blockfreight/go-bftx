afddce87b4efe0fcf2df2097dbddddca89bc655d
Julian Nuñez <julian.nunezm@gmail.com>
2017-03-14 20:54:56 +1100

    Add version library to manage the app version

---
+++ blockfreight/version/version.go
@@ -0,0 +1,81 @@
+// File: ./blockfreight/version/version.go
+// Summary: Application code for Blockfreight™ | The blockchain of global freight.
+// License: MIT License
+// Company: Blockfreight, Inc.
+// Author: Julian Nunez, Neil Tran, Julian Smith & contributors
+// Site: https://blockfreight.com
+// Support: <support@blockfreight.com>
+
+// Copyright 2017 Blockfreight, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// =================================================================================================================================================
+// =================================================================================================================================================
+//
+// BBBBBBBBBBBb     lll                                kkk             ffff                         iii                  hhh            ttt
+// BBBB``````BBBB   lll                                kkk            fff                           ```                  hhh            ttt
+// BBBB      BBBB   lll      oooooo        ccccccc     kkk    kkkk  fffffff  rrr  rrr    eeeee      iii     gggggg ggg   hhh  hhhhh   tttttttt
+// BBBBBBBBBBBB     lll    ooo    oooo    ccc    ccc   kkk   kkk    fffffff  rrrrrrrr eee    eeee   iii   gggg   ggggg   hhhh   hhhh  tttttttt
+// BBBBBBBBBBBBBB   lll   ooo      ooo   ccc           kkkkkkk        fff    rrrr    eeeeeeeeeeeee  iii  gggg      ggg   hhh     hhh    ttt
+// BBBB       BBB   lll   ooo      ooo   ccc           kkkk kkkk      fff    rrr     eeeeeeeeeeeee  iii   ggg      ggg   hhh     hhh    ttt
+// BBBB      BBBB   lll   oooo    oooo   cccc    ccc   kkk   kkkk     fff    rrr      eee      eee  iii    ggg    gggg   hhh     hhh    tttt    ....
+// BBBBBBBBBBBBB    lll     oooooooo       ccccccc     kkk     kkkk   fff    rrr       eeeeeeeee    iii     gggggg ggg   hhh     hhh     ttttt  ....
+//                                                                                                        ggg      ggg
+//   Blockfreight™ | The blockchain of global freight.                                                      ggggggggg
+//
+// =================================================================================================================================================
+// =================================================================================================================================================
+
+// Package version is a package that defines the current version of the project.
+package version
+
+const Maj = "0"
+const Min = "2"
+const Fix = "0"
+
+const Version = Maj+"."+Min+"."+Fix
+
+// =================================================
+// Blockfreight™ | The blockchain of global freight.
+// =================================================
+
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBB                    BBBBBBBBBBBBBBBBBBB
+// BBBBBBB                       BBBBBBBBBBBBBBBB
+// BBBBBBB                        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBB        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBB        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBB         BBBBBBBBBBBBBBBB
+// BBBBBBB                     BBBBBBBBBBBBBBBBBB
+// BBBBBBB                        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBBB        BBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBBBB       BBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBBB        BBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBB        BBB       BBBBB
+// BBBBBBB                       BBBB       BBBBB
+// BBBBBBB                    BBBBBBB       BBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+
+// ==================================================
+// Blockfreight™ | The blockchain for global freight.
+// ==================================================
