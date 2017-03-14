b66e2cb48a3607dc35cc3f31a3c8f84307a01f29
Julian Nuñez <julian.nunezm@gmail.com>
2017-03-14 20:52:59 +1100

    Add files for testing

---
+++ blockfreight/bft/crypto/crypto_test.go
@@ -0,0 +1,21 @@
+package crypto
+
+import(
+	"testing"
+
+	"github.com/blockfreight/blockfreight-alpha/blockfreight/bft/bf_tx"
+)
+
+func TestSign_BF_TX(t *testing.T) {
+	t.Log("Test on Sign_BF_TX function")
+	bftx := bf_tx.SetBF_TX("./files/bf_tx_example.json")
+    
+	bftx = Sign_BF_TX(bftx)
+
+    if bftx.Signature == "" {
+    	t.Error("Error on bf_tx.Signature")
+    }
+    if bftx.Verified == false {
+    	t.Error("Error on bf_tx.Verified")
+    }
+}
---
+++ blockfreight/bft/validator/validator_test.go
@@ -0,0 +1,17 @@
+package validator
+
+import (
+	"testing"
+
+	"github.com/blockfreight/blockfreight-alpha/blockfreight/bft/bf_tx"
+)
+
+func TestValidator(t *testing.T) {
+	t.Log("Test on validator function")
+	bf_tx := bf_tx.SetBF_TX("./files/bf_tx_example.json")
+    result := ValidateBf_Tx(bf_tx)
+    if result != "Success! [OK]" {
+    	t.Error("Error on result of TestValidator")
+    	t.Error(result)
+    }
+}
