990a940a16e89ef5a5b308b5248015c9b0a5c0e7
Julian Nuñez <julian.nunezm@gmail.com>
2017-03-17 01:17:49 +1100

    Modify filepath for test files

--- blockfreight/bft/validator/validator_test.go
+++ blockfreight/bft/validator/validator_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestValidator(t *testing.T) {
 	t.Log("Test on validator function")
-	bf_tx := bf_tx.SetBF_TX("./files/bf_tx_example.json")
+	bf_tx := bf_tx.SetBF_TX("../.././files/bf_tx_example.json")
     result := ValidateBf_Tx(bf_tx)
     if result != "Success! [OK]" {
     	t.Error("Error on result of TestValidator")
