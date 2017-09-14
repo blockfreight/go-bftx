401d68498c6d76246ee2049842d9e13d2e64f5f1
Julian Smith <julian.smith@blockfreight.com>
2017-09-14 23:50:44 +1000

    Update Golang ‘gofmt’
    Signed-off-by: Julian Smith <julian.smith@blockfreight.com>

--- lib/pkg/crypto/crypto_test.go
+++ lib/pkg/crypto/crypto_test.go
@@ -1,6 +1,6 @@
 package crypto
 
-import(
+import (
 	"testing"
 
 	"github.com/blockfreight/blockfreight-alpha/lib/app/bf_tx"
@@ -9,19 +9,19 @@ import(
 func TestSign_BF_TX(t *testing.T) {
 	t.Log("Test on Sign_BF_TX function")
 	bftx, err := bf_tx.SetBF_TX("../../../examples/bf_tx_example.json")
-    if err != nil {
-        t.Log(err.Error())
-    }
-    
-	bftx, err = Sign_BF_TX(bftx)
-    if err != nil {
-        t.Log(err.Error())
-    }
+	if err != nil {
+		t.Log(err.Error())
+	}
 
-    if bftx.Signature == "" {
-    	t.Error("Error on bf_tx.Signature")
-    }
-    if bftx.Verified == false {
-    	t.Error("Error on bf_tx.Verified")
-    }
+	bftx, err = Sign_BF_TX(bftx)
+	if err != nil {
+		t.Log(err.Error())
+	}
+
+	if bftx.Signature == "" {
+		t.Error("Error on bf_tx.Signature")
+	}
+	if bftx.Verified == false {
+		t.Error("Error on bf_tx.Verified")
+	}
 }
