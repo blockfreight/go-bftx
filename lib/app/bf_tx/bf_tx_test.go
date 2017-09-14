046e24c4b22a0993303534db4911c286c31bfffa
Gian Felipe <gianfelipe93@gmail.com>
2017-09-14 11:50:37 +1000

    Bugfix: fixing code after restructuring

--- lib/app/bf_tx/bf_tx_test.go
+++ lib/app/bf_tx/bf_tx_test.go
@@ -8,7 +8,7 @@ import (
 func TestSetBF_TX(t *testing.T) {
 	t.Log("Test on SetBF_TX function")
 	var prot BF_TX
-	bf_tx, err := SetBF_TX("../.././files/bf_tx_example.json")
+	bf_tx, err := SetBF_TX("../../../examples/bf_tx_example.json")
 	if err != nil {
         t.Log(err.Error())
     }
