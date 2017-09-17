7763bfb57015e306a5139dde366605c5c08d3d99
Gian Felipe <gianfelipe93@gmail.com>
2017-09-17 19:40:29 +1000

    Bugfix: fixing crypto.HashByteArrays test

--- test/pkg/common/common_test.go
+++ test/pkg/common/common_test.go
@@ -4,18 +4,17 @@ import (
   "testing"
   "bytes"
 
-	"github.com/blockfreight/blockfreight-alpha/lib/app/bf_tx"
 	"github.com/blockfreight/blockfreight-alpha/lib/pkg/common"
 )
 
 func TestHashByteArrays(t* testing.T){
   first_hash := []byte("firstHash")
   second_hash := []byte("secondHash")
+  result_expected := []byte{35, 63, 72, 164, 129, 98, 5, 123, 77, 35, 41, 21, 136, 230, 199, 208, 195, 68, 188, 65, 198, 199, 175, 43, 113, 168, 46, 95, 93, 208, 85, 227}
 
-  resultBftx := bf_tx.HashBF_TX_salt(first_hash, second_hash)
   resultCommon := common.HashByteArrays(first_hash, second_hash)
 
-  if  bytes.Compare(resultCommon, resultBftx) != 0 {
+  if  bytes.Compare(resultCommon, result_expected) != 0 {
     t.Error("Error on HashByteArrays!")
   }
 }
