0224936fdea0b1b470bb30381344beabb409a4bb
Gian Felipe <gianfelipe93@gmail.com>
2017-09-18 10:47:10 +1000

    Fixing gofmt issue

--- test/pkg/common/common_test.go
+++ test/pkg/common/common_test.go
@@ -1,21 +1,21 @@
 package common
 
 import (
-  "testing"
-  "bytes"
+	"bytes"
+	"testing"
 
 	"github.com/blockfreight/blockfreight-alpha/lib/pkg/common"
 )
 
-func TestHashByteArrays(t* testing.T){
-  t.Log("Test HashBytesArrays on Common Lib")
-  first_hash := []byte("firstHash")
-  second_hash := []byte("secondHash")
-  result_expected := []byte{35, 63, 72, 164, 129, 98, 5, 123, 77, 35, 41, 21, 136, 230, 199, 208, 195, 68, 188, 65, 198, 199, 175, 43, 113, 168, 46, 95, 93, 208, 85, 227}
+func TestHashByteArrays(t *testing.T) {
+	t.Log("Test HashBytesArrays on Common Lib")
+	first_hash := []byte("firstHash")
+	second_hash := []byte("secondHash")
+	result_expected := []byte{35, 63, 72, 164, 129, 98, 5, 123, 77, 35, 41, 21, 136, 230, 199, 208, 195, 68, 188, 65, 198, 199, 175, 43, 113, 168, 46, 95, 93, 208, 85, 227}
 
-  resultCommon := common.HashByteArrays(first_hash, second_hash)
+	resultCommon := common.HashByteArrays(first_hash, second_hash)
 
-  if  bytes.Compare(resultCommon, result_expected) != 0 {
-    t.Error("Error on HashByteArrays!")
-  }
+	if bytes.Compare(resultCommon, result_expected) != 0 {
+		t.Error("Error on HashByteArrays!")
+	}
 }
