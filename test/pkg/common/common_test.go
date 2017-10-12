28c92110d85424b2ceef158e6507b2567c7f926b
Gian Felipe <gianfelipe93@gmail.com>
2017-10-12 11:30:07 +1100

    Fixing golint issues

--- test/pkg/common/common_test.go
+++ test/pkg/common/common_test.go
@@ -9,13 +9,13 @@ import (
 
 func TestHashByteArrays(t *testing.T) {
 	t.Log("Test HashBytesArrays on Common Lib")
-	first_hash := []byte("firstHash")
-	second_hash := []byte("secondHash")
-	result_expected := []byte{35, 63, 72, 164, 129, 98, 5, 123, 77, 35, 41, 21, 136, 230, 199, 208, 195, 68, 188, 65, 198, 199, 175, 43, 113, 168, 46, 95, 93, 208, 85, 227}
+	firstHash := []byte("firstHash")
+	secondHash := []byte("secondHash")
+	resultExpected := []byte{35, 63, 72, 164, 129, 98, 5, 123, 77, 35, 41, 21, 136, 230, 199, 208, 195, 68, 188, 65, 198, 199, 175, 43, 113, 168, 46, 95, 93, 208, 85, 227}
 
-	resultCommon := common.HashByteArrays(first_hash, second_hash)
+	resultGenerated := common.HashByteArrays(firstHash, secondHash)
 
-	if bytes.Compare(resultCommon, result_expected) != 0 {
+	if bytes.Compare(resultGenerated, resultExpected) != 0 {
 		t.Error("Error on HashByteArrays!")
 	}
 }
