64b05825621103c5bbfaf5cb1b0e17c421ea02e2
Gian Felipe <gianfelipe93@gmail.com>
2017-10-27 12:26:00 +1100

    Add function to call Counterparty public service

--- lib/pkg/python/hello.py
+++ lib/pkg/python/hello.py
@@ -1,2 +1,19 @@
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
 def HelloPython():
-    return "Hello, Python!"
+    url = "http://public.coindaddy.io:4000/api/"
+    data = {}
+    data['content-type'] = 'application/json'   
+    auth = HTTPBasicAuth('rpc', '1234')
+
+    payload = {
+    "method": "get_running_info",
+    "params": {},
+    "jsonrpc": "2.0",
+    "id": 0,
+    }
+    response = requests.post(url, data=json.dumps(payload), headers=data, auth=auth)
+    
+    return response.text
