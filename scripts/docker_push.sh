f2b78848b7a83e070038422469acc7bf4bff985d
SeanTurner026 <sean.turner026@gmail.com>
2018-06-21 09:08:56 -0500

    testing travis CI/CD

--- scripts/docker_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push blockfreight/go-bftx:ci-cd
-# docker push blockfreight/go-bftx:${TRAVIS_COMMIT}
