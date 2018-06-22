804b0302661f69c0e8635ddbe7f2392ab4a2bd3b
SeanTurner026 <sean.turner026@gmail.com>
2018-06-22 14:19:02 -0500

    testing travis

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-IMG=blockfreight/go-bftx
+REPO=blockfreight/go-bftx
 
 TAG=ci-cd-`git log -1 --pretty=%h`
 # TAG=ci-cd-`echo ${TRAVIS_COMMIT}`
 
-ID=`docker build . -t ${IMG} | grep "Successfully built" | awk '{print $3;}'`
+ID=`docker build . -t ${REPO} | grep "Successfully built" | awk '{print $3;}'`
 
 echo "Successfully built ${ID}"
 
-docker tag ${ID} ${IMG}:${TAG}
+docker tag ${ID} ${REPO}:${TAG}
 
-echo "Successfully tagged ${ID}, Pushing ${IMG}:${TAG}"
+echo "Successfully tagged ${ID}, Pushing ${REPO}:${TAG}"
 
-docker push ${IMG}:${TAG}
+docker push ${REPO}:${TAG}
