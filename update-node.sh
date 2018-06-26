a6bed4c2a3f38ed874c7353843c997f323f365c5
SeanTurner026 <sean.turner026@gmail.com>
2018-06-26 13:31:56 -0500

    testing travis

--- update-node.sh
+++ update-node.sh
@@ -14,5 +14,5 @@ done
 # ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/resouces/blockfreight-statefulset.yaml > app.yaml;CONTAINER=`cat app.yaml | grep blockfreight/go-bftx: | awk '{print $2}' | sed -e 's#.*:\(\)#\1#'`;sed -i -- 's/<VALIDATOR_NAME>/'$validator_name'/g' *;sed -i -- 's/$CONTAINER/'$1'/g' *;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force" <<-'ENDSSH'
 # ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/resouces/blockfreight-statefulset.yaml > statefulset.yaml;python3 /home/$1/image_modifier.py $2 $3;kubectl apply -f statefulset.yaml && kubectl delete pods --all --grace-period=0 --force" <<-'ENDSSH'
 
-sed: can\'t read sedf2sN4V: Permission denied
-sed: -e expression #1, char 0: no previous regular expression
+# sed: can\'t read sedf2sN4V: Permission denied
+# sed: -e expression #1, char 0: no previous regular expression
