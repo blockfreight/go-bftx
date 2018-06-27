a3228395fee63271b7411844227c407d7a220db2
SeanTurner026 <sean.turner026@gmail.com>
2018-06-27 13:42:01 -0500

    testing travis

--- update-node.sh
+++ update-node.sh
@@ -9,15 +9,14 @@ do
 
     ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;\
     curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
-    sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
+    sed -i -- 's|<VALIDATOR_NAME>|$validator_name|g' app.yaml;\
     sed -i -- 's|'$CONTAINER'|'blockfreight/go-bftx:$1'|g' app.yaml;\
     kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;\
     echo old_tag: $CONTAINER;\
     echo new_tag: $1;\
-    echo app.yaml;\
-    rm app.yaml pre_app.yaml" <<-'ENDSSH'
+    cat app.yaml;\
+    rm app.yaml" <<-'ENDSSH'
 
-    
 ENDSSH
     ((index+=1))
 done
