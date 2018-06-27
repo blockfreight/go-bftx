9ee9cc494e7678c0f8196011d6231b51359cf994
SeanTurner026 <sean.turner026@gmail.com>
2018-06-27 13:10:47 -0500

    testing travis

--- update-node.sh
+++ update-node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CONTAINER=`curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml | grep blockfreight/go-bftx: | awk '{print $2}' | sed -e 's#.*:\(\)#\1#'`
+
 index=0
 for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
 do
