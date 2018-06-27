9f9c757cd6fc25ba483433f3ce17158866427cda
SeanTurner026 <sean.turner026@gmail.com>
2018-06-27 14:25:25 -0500

    testing travis

--- scripts/update-node.sh
+++ scripts/update-node.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # isolate previous tag as variable for sed replacement
-PREV_TAG=`curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml | grep blockfreight/go-bftx: | awk '{print $2}' | sed -e 's#.*:\(\)#\1#'`
+OLD_TAG=`curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml \|
+\ grep blockfreight/go-bftx: |
+\ awk '{print $2}' | 
+\ sed -e 's#.*:\(\)#\1#'`
+NEW_TAG=$1
 
 index=0
 for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTER_IP
@@ -11,13 +15,13 @@ do
     ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;\
     curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
     sed -i -- 's|<VALIDATOR_NAME>|$validator_name|g' app.yaml;\
-    sed -i -- 's|'$PREV_TAG'|'$1'|g' app.yaml;\
-    echo old_tag: $PREV_TAG
-    echo new_tag: $1
-    cat app.yaml | grep $1
+    sed -i -- 's|'$OLD_TAG'|'$NEW_TAG'|g' app.yaml;\
+    echo old_tag: $OLD_TAG;\
+    echo new_tag: $NEW_TAG;\
+    cat app.yaml | grep $NEW_TAG | awk '{print $1 $2}';\
     kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;\
     rm app.yaml" <<-'ENDSSH'
 
 ENDSSH
     ((index+=1))
-done
+done        
