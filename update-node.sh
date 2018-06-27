8d5f606d89167714d85100d863ea5433993b687e
SeanTurner026 <sean.turner026@gmail.com>
2018-06-27 13:34:31 -0500

    testing travis

--- update-node.sh
+++ update-node.sh
@@ -7,15 +7,15 @@ for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTE
 do
     validator_name=bftx${index}
 
-    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;
-    \ curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;
-    \ sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;
-    \ sed -i -- 's|'$CONTAINER'|'blockfreight/go-bftx:$1'|g' app.yaml;
-    \ echo old_tag: $CONTAINER;
-    \ echo new_tag: $1;
-    \ cat app.yaml;
-    \ kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;
-    \ rm app.yaml" <<-'ENDSSH'
+    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;\
+    curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;\
+    sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml;\
+    sed -i -- 's|'$CONTAINER'|'blockfreight/go-bftx:$1'|g' app.yaml;\
+    kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;\
+    echo old_tag: $CONTAINER;\
+    echo new_tag: $1;\
+    echo app.yaml;\
+    rm app.yaml pre_app.yaml" <<-'ENDSSH'
 
     
 ENDSSH
