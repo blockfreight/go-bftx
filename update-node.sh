08665a5c01dcc37f935af4cf66ffd55c9b68906f
SeanTurner026 <sean.turner026@gmail.com>
2018-06-27 13:08:32 -0500

    testing travis

--- update-node.sh
+++ update-node.sh
@@ -5,12 +5,14 @@ for validator in $BFTX0_MASTER_IP $BFTX1_MASTER_IP $BFTX2_MASTER_IP $BFTX3_MASTE
 do
     validator_name=bftx${index}
 
-    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;CONTAINER=`cat app.yaml | grep blockfreight/go-bftx: | awk '{print $2}' | sed -e 's#.*:\(\)#\1#'`;sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml && sed -i -- 's|'$CONTAINER'|'blockfreight/go-bftx:$1'|g' app.yaml;echo old_tag: $CONTAINER;echo tag: $1;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;rm app.yaml pre_app.yaml" <<-'ENDSSH'
+    ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;sed -i -- 's/<VALIDATOR_NAME>/$validator_name/g' app.yaml && sed -i -- 's|'$2'|'blockfreight/go-bftx:$1'|g' app.yaml;echo old_tag: $CONTAINER;echo tag: $1;cat app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;rm app.yaml pre_app.yaml" <<-'ENDSSH'
 
     
 ENDSSH
     ((index+=1))
 done
+
+# CONTAINER=`curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml | grep blockfreight/go-bftx: | awk '{print $2}' | sed -e 's#.*:\(\)#\1#'`
 
 # ssh -oStrictHostKeyChecking=no $BLOCKFREIGHT_SSH_USER@$validator "env validator_name=$validator_name;echo $validator_name;curl https://raw.githubusercontent.com/blockfreight/tools/master/blockfreightnet-kubernetes/examples/blockfreight/app.yaml > app.yaml;CONTAINER=`cat home/$2/app.yaml | grep blockfreight/go-bftx: | awk '{print $2}'`;sed -i -- 's|<VALIDATOR_NAME>|'$validator_name'|g' app.yaml;sed -i -- 's|'$CONTAINER'|'blockfreight/go-bftx:$1'|g' app.yaml;kubectl apply -f app.yaml && kubectl delete pods --all --grace-period=0 --force;cat home/$2/app.yam;" <<-'ENDSSH'
 
